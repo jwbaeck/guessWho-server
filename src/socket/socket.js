4ad5963071f9a3b85a4668b888347d30b2db6db9
jwbaeck <jwbaeck97@gmail.com>
2024-03-12 21:44:22 +0900

    feat: 사용자 등록 및 정원 설정

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,26 @@
 const socketIo = require("socket.io");
 
+const users = new Map();
+const maxCapacity = 4;
+
+function sendUserListToRoom(io, room) {
+  const userList = Array.from(users.values())
+    .filter(user => user.room === room)
+    .map(user => ({ id: user.id, name: user.name }));
+
+  io.to(room).emit("updateUsers", userList);
+}
+
+function checkCapacityAndSendResponse(io, room) {
+  if (users.size >= maxCapacity) {
+    const userList = Array.from(users.values())
+      .filter(user => user.room === room)
+      .map(user => ({ id: user.id, name: user.name }));
+
+    io.to(room).emit("maxCapacityReached", userList);
+  }
+}
+
 function setUpSocketServer(server) {
   const io = socketIo(server, {
     cors: {
@@ -11,8 +32,17 @@ function setUpSocketServer(server) {
   io.on("connect", socket => {
     console.log("클라이언트 소켓 연결 성공");
 
-    socket.on("test", req => {
-      console.log(req);
+    socket.on("submitNickname", nickname => {
+      const room = 1;
+
+      users.set(socket.id, {
+        id: socket.id,
+        name: nickname,
+        room,
+      });
+
+      sendUserListToRoom(io, room);
+      checkCapacityAndSendResponse(io, room);
     });
   });
 
